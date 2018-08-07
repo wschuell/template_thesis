a27a6c5a4e49687f322122d1a6302448a8b625be
William Schueller <william.schueller@gmail.com>
2018-08-07 02:35:08 +0200

    fixed git checkout overleaf

--- pull_from_overleaf.sh
+++ pull_from_overleaf.sh
@@ -3,6 +3,7 @@ if [ ! $(git rev-parse --verify overleaf) ]; then
 	echo "Please provide overleaf git repository URL:"
 	read URL
 	git remote add overleaf $URL || exit 1
+	git checkout -b overleaf
 fi
 git checkout overleaf && git pull overleaf master
 git checkout master
